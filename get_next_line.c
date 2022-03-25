305624a9d89063bad22216a990ccd1af0fe4b379
massaaki <eu@massaaki.com>
2022-03-25 22:07:06 +0100

    feat: starting base project

--- get_next_line.c
+++ get_next_line.c
@@ -1,30 +1,9 @@
 #include "get_next_line.h"
 
-/*
-** Description:
-** Function which a new line read a file descriptor
-** 
-** Parameters:
-** fd: File descriptor
-** **line: value to read
-**
-** Returns:
-** 1: if a line has been read
-** 0: EOF reached
-** -1: Error
-*/
-
 #include <stdio.h>
 
 int get_next_line(int fd, char **line)
 {
-	char	buffer[BUFFER_SIZE + 1];
-
-	if (fd < 0 || !line || BUFFER_SIZE <= 0)
-		return (-1);
-
-	if(read(fd, buffer, BUFFER_SIZE) > 0)
-		printf("\"%s\"", buffer);
 
 	return (0);
 }
