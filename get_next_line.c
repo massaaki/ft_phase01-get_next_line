a1f47dcb4b3aa41461cfea084b415902f89e4c46
massaaki <eu@massaaki.com>
2022-03-26 16:21:07 -0300

    feat: read line by line from file

--- get_next_line.c
+++ get_next_line.c
@@ -4,6 +4,26 @@
 
 int get_next_line(int fd)
 {
-	printf("getNextLine \n");
+	char buffer[BUFFER_SIZE + 1];
+	int file_return;
+
+	// If successful, the number of bytes actually read is returned
+	// EOF, returns 0
+	// Fail, returns -1
+
+	/*
+	 * file_return = read(fd, buffer, BUFFER_SIZE);
+	 * if EOF retuns 0, we can put read in a while loop
+	 */
+
+	while ((file_return = read(fd, buffer, BUFFER_SIZE)) > 0) 
+	{
+		//if entered here, means that we have more buffer
+		buffer[file_return] = '\0'; // ensure end of string
+		printf("BUFF: %s\n", buffer);
+		printf("*******************\n");
+	}
+
+		
 	return (0);
 }
