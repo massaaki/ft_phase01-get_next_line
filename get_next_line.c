b64d53ba23445f39fb420fea82965afba026b3b9
massaaki <eu@massaaki.com>
2022-03-30 20:39:23 -0300

    fix: retrieve comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: massaaki <massaaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:10:12 by massaaki          #+#    #+#             */
-/*   Updated: 2022/03/30 16:52:00 by massaaki         ###   ########.fr       */
+/*   Updated: 2022/03/30 20:35:51 by massaaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,16 @@
 
 char	*ft_split_n(char **accumulator, int last_line);
 
-int test = 0;
-
 char *get_next_line(int fd)
 {
-	char			*current_buffer;
+	char			current_buffer[BUFFER_SIZE + 1];
 	int				file_return;
 	static char		*accumulator;
 	char			*current_line;
 	char			*ptr_n;
 
-	if (!accumulator)
+	if (!accumulator) 
 		accumulator = ft_strdup("");
-	current_buffer = malloc((BUFFER_SIZE + 1) * sizeof(current_buffer));
 
 	file_return = BUFFER_SIZE;
 	while (file_return > 0)
@@ -36,29 +33,25 @@ char *get_next_line(int fd)
 		current_buffer[file_return] = '\0';
 
 		if (file_return > 0 || ft_strchr(accumulator, '\n'))
-		{	
+		{
+			// printf("alocou\n"); //Esta entrando 2x no loop
 			accumulator = ft_strjoin(accumulator, current_buffer);
-			ptr_n = ft_strchr(accumulator, '\n');
+					ptr_n = ft_strchr(accumulator, '\n');
 			if (ptr_n)
 			{
 				current_line = ft_split_n(&accumulator, 0);
-				test = 1;
-				free(current_buffer);
 				return (current_line);
 			}
-
 		}
 		else if ((file_return == 0 && ft_strlen(accumulator) > 0))
 		{
 			ptr_n = ft_strchr(accumulator, '\n');
 			current_line = ft_split_n(&accumulator, 1);
-			free(current_buffer);
 			return (current_line);
-
 		}
 	}
 	free(accumulator);
-	free(current_buffer);
+	accumulator = NULL;
 	return (NULL);
 }
 
