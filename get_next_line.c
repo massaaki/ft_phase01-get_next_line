8dda6e0ca034d4f953a09e54ecf395770347a5eb
massaaki <eu@massaaki.com>
2022-03-31 09:30:06 -0300

    fix: solve leaks in accumulator casting free before allocate memory again

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: massaaki <massaaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:10:12 by massaaki          #+#    #+#             */
-/*   Updated: 2022/03/30 20:35:51 by massaaki         ###   ########.fr       */
+/*   Updated: 2022/03/31 09:26:16 by massaaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,33 @@ char *get_next_line(int fd)
 	char			current_buffer[BUFFER_SIZE + 1];
 	int				file_return;
 	static char		*accumulator;
-	char			*current_line;
+	char 			*temp_accumulator;
+	char 			*current_line;
 	char			*ptr_n;
 
 	if (!accumulator) 
 		accumulator = ft_strdup("");
 
 	file_return = BUFFER_SIZE;
-	while (file_return > 0)
+	// while (file_return > 0)
+	while (file_return > 0 || ft_strchr(accumulator, '\n'))
 	{
 		file_return = read(fd, current_buffer, BUFFER_SIZE);
 		current_buffer[file_return] = '\0';
+		
+		temp_accumulator = ft_strjoin(accumulator, current_buffer);
+		free(accumulator);
+		accumulator = malloc(ft_strlen(temp_accumulator) * sizeof(char) + 1);
+		ft_strlcpy(accumulator, temp_accumulator, ft_strlen(temp_accumulator) + 1);
+		free(temp_accumulator);
 
-		if (file_return > 0 || ft_strchr(accumulator, '\n'))
+		ptr_n = ft_strchr(accumulator, '\n');
+		if (ptr_n)
 		{
-			// printf("alocou\n"); //Esta entrando 2x no loop
-			accumulator = ft_strjoin(accumulator, current_buffer);
-					ptr_n = ft_strchr(accumulator, '\n');
-			if (ptr_n)
-			{
-				current_line = ft_split_n(&accumulator, 0);
-				return (current_line);
-			}
+			current_line = ft_split_n(&accumulator, 0);
+			return (current_line);
 		}
-		else if ((file_return == 0 && ft_strlen(accumulator) > 0))
+		if ((file_return == 0 && ft_strlen(accumulator) > 0))
 		{
 			ptr_n = ft_strchr(accumulator, '\n');
 			current_line = ft_split_n(&accumulator, 1);
@@ -82,6 +85,7 @@ char	*ft_split_n(char **accumulator, int last_line)
 	}
 	temp_accumulator = malloc((len - i) * sizeof(char));
 	ft_strlcpy(temp_accumulator, *(accumulator) + i + 1, (len - i + 1));
+	free(*accumulator);
 	*accumulator = malloc(len - i * sizeof(char));
 	ft_strlcpy(*accumulator, temp_accumulator, ft_strlen(temp_accumulator) + 1);
 	free(temp_accumulator);
