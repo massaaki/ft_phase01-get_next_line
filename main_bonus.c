b37ee628df6c7f5207f88f87df167643acb40fd2
massaaki <eu@massaaki.com>
2022-04-05 21:52:09 -0300

    test: add test to call gnl with different files

---
+++ main_bonus.c
@@ -0,0 +1,42 @@
+#include "get_next_line_bonus.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void testBonus(void);
+
+int main(void)
+{
+
+	testBonus();
+	
+	return (0);
+}
+
+void testBonus(void)
+{
+	FILE *file1;
+	FILE *file2;
+
+	int i;
+	char *current_line1;
+	char *current_line2;
+
+
+	i = 0;
+	file1 = fopen("./file-tests/str1", "r");
+	file2 = fopen("./file-tests/str2", "r");
+
+
+	current_line1 = get_next_line(file1->_file);
+
+	current_line2 = get_next_line(file2->_file);
+
+	current_line1 = get_next_line(file1->_file);
+	// current_line1 = get_next_line(file1->_file);
+
+
+	fclose(file1);
+	fclose(file2);
+
+}
