dbd7a35df5c1521a1d8966cded7618023c5d47fc
massaaki <eu@massaaki.com>
2022-04-06 16:25:31 -0300

    feat: ensure gnl_bonus split when find n updating accumulator and returning the line

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -6,7 +6,7 @@
 /*   By: massaaki <massaaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:10:12 by massaaki          #+#    #+#             */
-/*   Updated: 2022/04/05 21:55:33 by massaaki         ###   ########.fr       */
+/*   Updated: 2022/04/06 16:23:31 by massaaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void update_list(struct list **accumulator, int fd);
 void ft_join_accumulator(char **accumulator, char *current_buffer);
 void delete_fd(struct list **accumulator, int fd);
+char *ft_split_n(char **accumulator, char *ptr_n);
 
 char *get_next_line(int fd)
 {
@@ -23,7 +24,6 @@ char *get_next_line(int fd)
 	static struct list	*accumulator;
 	struct list 		*current;
 	int file_return;
-	char				*current_line;
 
 	if (!accumulator)
 	{
@@ -46,24 +46,14 @@ char *get_next_line(int fd)
 		current_buffer[file_return] = '\0';
 		ft_join_accumulator(&(current->buffer), current_buffer);
 
-		printf("FD: %d\n", fd);
-		printf("accumulator: '%s'\n", current->buffer);
-		printf("-------\n");
+		if (ft_strchr(current->buffer, '\n'))
+			return ft_split_n(&current->buffer, ft_strchr(current->buffer, '\n'));
 
-		if (ft_strchr(current->buffer, '-'))
-		{
-			/*
-			 * TODO:
-			 * 1 - Split '-'
-			 * 2 - return content before '-'
-			 * 3 - realloc and keep content after '-'
-			 */
-			break;
-		}
 	}
 
 	if(file_return == 0)
 		delete_fd(&accumulator, fd);
+
 	return (NULL);
 }
 
@@ -83,7 +73,7 @@ void delete_fd(struct list **accumulator, int fd)
 	else
 		previous = to_exclude->next;
 
-	printf("EXCLUIR: %d\n", to_exclude->fd);
+	// printf("EXCLUIR: %d\n", to_exclude->fd);
 	free(to_exclude);
 }
 
@@ -131,4 +121,29 @@ void ft_join_accumulator(char **accumulator, char *current_buffer)
 	free(tmp_acc);
 
 	//get rest here
+}
+
+
+/*
+* return line and update accumulator
+*/
+char *ft_split_n(char **accumulator, char *ptr_n)
+{
+	int length;
+	char *line;
+	char *rest;
+
+	length = ptr_n - *accumulator + 1;
+	line = malloc(length * sizeof(char) + 1);
+	rest = malloc(ft_strlen(ptr_n) * sizeof(char));
+	
+	ft_strlcpy(line, *accumulator, length + 1);
+	ft_strlcpy(rest, (ptr_n + 1), ft_strlen(ptr_n) + 1);
+
+	free(*accumulator);
+	*accumulator = malloc(ft_strlen(rest) * sizeof(char) + 1);
+	ft_strlcpy(*accumulator, rest, ft_strlen(rest) + 1);
+	free(rest);
+
+	return (line);
 }
