2a5f0bd8df675272055b0848e953cbe24fba3c7f
massaaki <eu@massaaki.com>
2022-04-11 12:46:40 -0300

    feat: ensure current_buffer can alloc memory for 10000000

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,55 @@
 /*   By: massaaki <massaaki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 16:10:12 by massaaki          #+#    #+#             */
-/*   Updated: 2022/04/09 16:28:24 by massaaki         ###   ########.fr       */
+/*   Updated: 2022/04/11 12:45:03 by massaaki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_split_n(char **accumulator, int last_line);
-void	ft_join_accumulator(char **accumulator, char *current_buffer);
-void	ft_keep_rest_accumulator(char **accumulator, int len, int i);
-int		ft_manage_split(char **accumulator, char **current, int file_return);
+char *ft_split_n(char **accumulator, int last_line);
+void ft_join_accumulator(char **accumulator, char *current_buffer);
+void ft_keep_rest_accumulator(char **accumulator, int len, int i);
+int ft_manage_split(char **accumulator, char **current, int file_return);
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
-	char			current_buffer[BUFFER_SIZE + 1];
-	int				file_return;
-	static char		*accumulator;
-	char			*current_line;
+	char *current_buffer;
+	int file_return;
+	static char *accumulator;
+	char *current_line;
 
 	if (!accumulator)
 		accumulator = ft_strdup("");
 	file_return = BUFFER_SIZE;
 	while (file_return > 0 || ft_strchr(accumulator, '\n'))
 	{
+		current_buffer = malloc(BUFFER_SIZE * sizeof(char) + 1);
 		file_return = read(fd, current_buffer, BUFFER_SIZE);
+		if (file_return < 0)
+		{
+			free(current_buffer);
+			return (NULL);
+		}
 		current_buffer[file_return] = '\0';
 		ft_join_accumulator(&accumulator, current_buffer);
+		free(current_buffer);
 		if (ft_manage_split(&accumulator, &current_line, file_return) == 1)
 			return (current_line);
 	}
 	free(accumulator);
-	accumulator = NULL;
-	return (NULL);
+	return ((accumulator = NULL), NULL);
 }
 
 /*
  * Receive accumulator and if is last line or not
  * returns current_line and update accumulator with rest
-*/
-char	*ft_split_n(char **accumulator, int last_line)
+ */
+char *ft_split_n(char **accumulator, int last_line)
 {
-	char	*line;
-	int		len;
-	int		i;
+	char *line;
+	int len;
+	int i;
 
 	i = 0;
 	len = ft_strlen(*accumulator);
@@ -58,7 +64,7 @@ char	*ft_split_n(char **accumulator, int last_line)
 		{
 			line = malloc(i * sizeof(char) + 2);
 			ft_strlcpy(line, *accumulator, i + 2);
-			break ;
+			break;
 		}
 		else if (last_line == 1)
 		{
@@ -78,10 +84,10 @@ char	*ft_split_n(char **accumulator, int last_line)
  * and current_buffer
  * Then gets the correcty length
  * Finally realloc and copy correcty to avoid leaks
-*/
-void	ft_join_accumulator(char **accumulator, char *current_buffer)
+ */
+void ft_join_accumulator(char **accumulator, char *current_buffer)
 {
-	char	*tmp_acc;
+	char *tmp_acc;
 
 	tmp_acc = ft_strjoin(*accumulator, current_buffer);
 	free(*accumulator);
@@ -93,12 +99,12 @@ void	ft_join_accumulator(char **accumulator, char *current_buffer)
 /*
  * Remove content before \n and realoc the rest in accumulator
  */
-void	ft_keep_rest_accumulator(char **accumulator, int len, int i)
+void ft_keep_rest_accumulator(char **accumulator, int len, int i)
 {
-	char	*temp_accumulator;
+	char *temp_accumulator;
 
 	if (ft_strlen(*accumulator) == 0)
-		return ;
+		return;
 	temp_accumulator = malloc((len - i) * sizeof(char));
 	ft_strlcpy(temp_accumulator, *(accumulator) + i + 1, (len - i));
 	free(*accumulator);
@@ -108,11 +114,11 @@ void	ft_keep_rest_accumulator(char **accumulator, int len, int i)
 }
 
 /*
-* Manage split to call when is the eof or not
-*/
-int	ft_manage_split(char **accumulator, char **line, int file_return)
+ * Manage split to call when is the eof or not
+ */
+int ft_manage_split(char **accumulator, char **line, int file_return)
 {
-	char	*ptr_n;
+	char *ptr_n;
 
 	ptr_n = ft_strchr(*accumulator, '\n');
 	if (ptr_n)
